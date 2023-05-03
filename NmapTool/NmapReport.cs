03688537ef05949f65ba4b66ce086be4ff6959da
Anton <Anton@INFINUM-LAPTOP>
2023-05-03 21:43:38 +0300

    Добавьте файлы проекта.

---
+++ NmapTool/NmapReport.cs
@@ -0,0 +1,25 @@
+﻿using NmapXmlParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NmapTool
+{
+    internal class NmapReport
+    {
+        public static nmaprun Read(string filePath)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(nmaprun));
+            var result = default(nmaprun);
+
+            using (var xmlStream = new StreamReader(filePath))
+            {
+                result = xmlSerializer.Deserialize(xmlStream) as nmaprun;
+            }
+            return result;
+        }
+    }
+}
---
+++ NmapTool/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NmapTool;
+
+var result = NmapReport.Read(args[0]);
+
+Console.WriteLine(result.args);
